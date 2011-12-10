15ee66405b219a782b1e6da169ac08769bbcf149
Jason <vision@myirc.net>
2011-12-10 19:36:29 +0200

    commit tou teo se ask1 kai 2.

--- lab1ex1.m
+++ lab1ex1.m
@@ -1,47 +1,96 @@
 
-% n=-4:6;
-% x=zeros(1,length(n));
-% 
-%  for i=1:length(n)
-%      if(n(i)==0||n(i)==1)
-%          x(i)=-2;
-%      elseif(n(i)==-2||n(i)==-1||n(i)==2||n(i)==3)
-%          x(i)=1;
-%      else
-%          x(i)=0;
-%      end
-%  end
-% 
-% figure(1);
-% stem(n,x)
-% ylim([-2 2]);  
-n=[1:5];
-x=zeros(1,5)
-max =2;
-for i=1:5
-    x(i)=input('Give the value of x:');
-    if (max < x(i))
-        max = x(i);
-    end
+n=-4:6;
+x=zeros(1,length(n));
+%Kataskeuh tou shmatos ths eikonas.
+ for i=1:length(n)
+     if(n(i)==0||n(i)==1)
+         x(i)=-2;
+     elseif(n(i)==-2||n(i)==-1||n(i)==2||n(i)==3)
+         x(i)=1;
+     else
+         x(i)=0;
+     end
+ end
+%Apeikonish tou prwtou simatos {x[n]}
+figure(1);
+subplot(2,1,1)
+stem(n,x,'filled');
+ylim([-5 2]);
+title('{x[n]}');
+xlabel('n');
+ylabel('x[n]');
 
+
+
+%Apeikonish tou prwtou simatos {x[n-2]}
+ for i=1:length(n)
+     if(n(i)==2||n(i)==3)
+         x2(i)=-2;
+     elseif(n(i)==0||n(i)==1||n(i)==4||n(i)==5)
+         x2(i)=1;
+     else
+         x2(i)=0;
+     end
+ end
+ %Apeikonish tou prwtou simatos {x[n]}
+subplot(2,1,2)
+stem(n,x2,'filled');
+ylim([-5 2]);
+title('{x[n-2]}');
+xlabel('n');
+ylabel('x[n-2]');
+
+%--------------------------------------------------------------------------
+
+%b)
+n=[1:20];
+x=randperm(length(n));
+max=x(1);
+for i=1:20
+ if x(i)>max
+ max=x(i);
+ end
 end
-max = max +2;
-figure(1)
-stem(n,x);
-xlim([-10 10])
-ylim([-max max]);
+
+min=x(1);
+for i=1:20
+ if x(i)<min
+ min=x(i);
+ end
+end
 
 figure(2)
-stem(-n,x);
-ylim([-max max]);
-xlim([-10 10]);
+subplot(2,2,1);
+stem(n,x,'filled');
+title('{ x[n] }');
+xlabel('n');
+ylabel('x[n]');
+ylim([min max]);
+xlim([-length(n)-1 length(n)+1])
 
-figure(3)
-stem(2*n,x);
-ylim([-max max]);
-xlim([-10 10]);
 
-figure(4)
-stem(3*n,x);
-ylim([-max max]);
-xlim([-10 10]);
+subplot(2,2,2);
+stem(-n,x,'filled');
+title('{ x[-n] }');
+xlabel('n');
+ylabel('x[-n]');
+ylim([min max]);
+xlim([-length(n)-1 length(n)+1])
+
+
+subplot(2,2,3);
+stem(2*n,x,'filled');
+title('{ x[2n] }');
+xlabel('n');
+ylabel('x[2n]');
+ylim([min max]);
+xlim([-length(n)-1 2*length(n)+1])
+
+
+subplot(2,2,4);
+stem(3*n,x,'filled');
+title('{x[3n]}');
+xlabel('n');
+ylabel('x[3n]');
+ylim([min max]);
+xlim([-length(n)-1 3*length(n)+1])
--- lab1ex2.m
+++ lab1ex2.m
@@ -1,29 +1,65 @@
+
 %% ASKHSH 2 
 %% Erwthma a)
-n=1:5;%%Xronos tou simatos
- x=rand(1,5);  % dhmiourgoume ena tyxaio dianisma.
-% 
- N=length(x);
+clear all;
+n=[1:40];
+x=randperm(length(n));
+ N=length(n);
 
+ 
+ 
 for k=0:N-1
 for n=0:N-1
 X1(n+1)=x(n+1).*exp(-j*2*pi*n*k/N);
 end
 X(k+1)=sum(X1);
 end
-X
- Z=fft(x);  % tsekaroume an ta apotelesmata mas sumpiptoun me thn etoimh sinarthsh matlab.
+X;
+n=1:40;%%Xronos pou orizetai to shma
+figure(1);
+subplot(2,1,1)
+stem(n,X,'filled')
+ title('Metasximatismos fourier kata kataskeuh.');
+ xlabel('n');
+ ylabel('F(x[n])');
+
+ Z=fft(x);  % Elenxoume an ta apotelesmata mas sumpiptoun me thn etoimh sinarthsh matlab
+ 
+ subplot(2,1,2)
+ stem(n,Z,'filled') 
+ title('Metasximatismos fourier me tin etimh sinartish fft');
+  xlabel('n');
+ ylabel('F(x[n])');
+
+ 
+ 
  
  %% erwthma b)
+
  for n=0:N-1
      for k=0:N-1
          Xn1(k+1)=Z(k+1)*exp(j*2*pi*n*k/N);
      end
-         Xn(n+1)=sum(Xn1);
+         X1(n+1)=sum(Xn1);
  end
-         
- Xn=Xn/N;
- 
+ for i=0:N-1
+ X1(i+1)=X1(i+1)/N;
+ end
+   
+   n=1:40;
+  figure(3);
+  subplot(2,1,1);
+ stem(n,x,'filled');
+  title('Arxiko shma x[n]');
+ xlabel('n');
+ ylabel('x[n]');
 
+subplot(2,1,2);
+ stem(n,X1,'filled')
+  title('Arxiko shma x[n] epeita apo anastrofo metasxhmatismo Fourier');
+ xlabel('n');
+ ylabel('F^-1(x[n])');
+
+ 
 
 
